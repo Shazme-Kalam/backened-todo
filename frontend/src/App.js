9a88491471f2370da99e5b6131efa1f9bd119030
Shazme-Kalam <miss.shazmekalam66@gmail.com>
2024-06-02 02:45:39 +0500

    working on API

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { useState } from 'react'
+import axios from "axios"
+function App  () {
+ const [title, settitle] = useState("")
+ const [desc, setdesc] = useState("")
+const handleSubmit = async (e) => {
+  e.preventDefault()
+console.log("first", title , desc)
+const data = { 
+  title,
+  desc
+}
+const resp = await axios.post("http://localhost:5000/todo/add", data)
+}
+// await fetch("http://localhost:5000/todo/add", {
+//   method : postMessage,
+//   body: JSON.stringify(data)
+// })
+// .then(res => res.json())
+// .then(data =>{
+//   console.log(data)
+// })
+
+return (
+  <form onSubmit={handleSubmit}>
+    <label for="title">title:</label><br />
+    <input type="text" id="title" name="title" onChange={(e) => settitle(e.target.value)} /><br />
+    <label for="desc">desc:</label><br />
+    <input type="text" id="desc" name="desc" onChange={(e) => setdesc(e.target.value)} /><br /><br />
+    <input type="submit" value="Submit" />
+  </form>
+);
+}
+export default App;
